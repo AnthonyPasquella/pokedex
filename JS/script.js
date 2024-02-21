fcd10bea94e9707563a95772c16fbcac5d47a047
AnthonyPasquella <apasquella@gmail.com>
2024-02-21 11:51:46 -0500

    updated for exercise 1.2 - JS

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,18 @@
-alert('Hello world');
-var favroiteFood = 'milksteak';
-document.write(favroiteFood)
+
+let pokemonList =[
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Pidgey',
+        height: .3,
+        type: ['flying', 'normal']
+    },
+    {
+        name: 'Psyduck',
+        height: .8,
+        type: ['water']
+    }
+];
