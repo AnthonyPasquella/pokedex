f6b9cc94a4d626939f7b39ebec127dadaacf61c5
AnthonyPasquella <apasquella@gmail.com>
2024-03-04 21:39:25 -0500

    changed the for to forEach() per 1.5.1

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,5 @@
 
 
-
 let pokemonList =[
     {
         name: 'Bulbasaur',
@@ -22,16 +21,33 @@ let pokemonList =[
     }
 ];
 
-for (let i=0; i < pokemonList.length; i++)
-    if (pokemonList[i].height > 0.7){
-        document.write('<p>', pokemonList[i].name + ' - ' + pokemonList[i].height + ' - Whoa, that is big! </p>')
-    }else if (pokemonList[i].height <= 0.7 && pokemonList[i].height > 0.3 ){
-        document.write('<p>', pokemonList[i].name + ' - ' + pokemonList[i].height + '- Meh. </p>')
-    }else {document.write('<p>', pokemonList[i].name + ' - ' + pokemonList[1].height + ' - I have seen bigger. </p>')}
+//for (let i=0; i < pokemonList.length; i++)
+  //  if (pokemonList[i].height > 0.7){
+    //    document.write('<p>', pokemonList[i].name + ' - ' + pokemonList[i].height + ' - Whoa, that is big! </p>')
+    //}else if (pokemonList[i].height <= 0.7 && pokemonList[i].height > 0.3 ){
+      //  document.write('<p>', pokemonList[i].name + ' - ' + pokemonList[i].height + '- Meh. </p>')
+    //}else {document.write('<p>', pokemonList[i].name + ' - ' + pokemonList[1].height + ' - I have seen bigger. </p>')}
 
-    for (let i=0; i < pokemonList.length; i++)
-    if (pokemonList[i].weight > 7){
-        document.write('<p>', pokemonList[i].name + ' - ' + pokemonList[i].weight + ' - A Real Heavyweight! </p>')
-    }else if (pokemonList[i].weight <= 7 && pokemonList[i].weight > 3 ){
-        document.write('<p>', pokemonList[i].name + ' - ' + pokemonList[i].weight + '- Middleweight Contender. </p>')
-    }else {document.write('<p>', pokemonList[i].name + ' - ' + pokemonList[1].weight + ' - Super Featherweight. </p>')}
+   // for (let i=0; i < pokemonList.length; i++)
+  //  if (pokemonList[i].weight > 7){
+ //       document.write('<p>', pokemonList[i].name + ' - ' + pokemonList[i].weight + ' - A Real Heavyweight! </p>')
+  //  }else if (pokemonList[i].weight <= 7 && pokemonList[i].weight > 3 ){
+   //     document.write('<p>', pokemonList[i].name + ' - ' + pokemonList[i].weight + '- Middleweight Contender. </p>')
+  //  }else {document.write('<p>', pokemonList[i].name + ' - ' + pokemonList[1].weight + ' - Super Featherweight. </p>')}
+
+  pokemonList.forEach(function(pokemon){
+    if (pokemon.height > 0.7){
+        document.write('<p>', pokemon.name + ' - ' + pokemon.height + 'm - Whoa, that is big! </p>')
+        }else if (pokemon.height <= 0.7 && pokemon.height > 0.3 ){
+           document.write('<p>', pokemon.name + ' - ' + pokemon.height + 'm - Meh. </p>')
+        }else {document.write('<p>', pokemon.name + ' - ' + pokemon.height + 'm - I have seen bigger. </p>')}
+  });
+
+    pokemonList.forEach(function(pokemon){
+   if (pokemon.weight > 7){
+        document.write('<p>', pokemon.name + ' - ' + pokemon.weight + 'kg - A Real Heavyweight! </p>')
+        }else if (pokemon.weight <= 7 && pokemon.weight > 3 ){
+        document.write('<p>', pokemon.name + ' - ' + pokemon.weight + 'kg - Middleweight Contender. </p>')
+        }else {document.write('<p>', pokemon.name + ' - ' + pokemon.weight + 'kg - Super Featherweight. </p>')}
+    })
+ 
